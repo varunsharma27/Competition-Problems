dbafdeac55e9f5ec1268e8ed2d55199cd257d417
Varun Sharma <contactvarun27@gmail.com>
2016-07-27 22:32:08 +0530

    Update main.cpp

--- Milkshake-Problem/main.cpp
+++ Milkshake-Problem/main.cpp
@@ -3,27 +3,11 @@
 #include <algorithm>
 #include <vector>
 #include"milkshake.h"
-//#include"Test_Vector_Find.h"
-
 
 using namespace std;
 
-int f(int);
 int main(int argc, char **argv)
 {
 	milk();
-	//zoo();
-}
-int f(int a)
-{
-	return a>20?(10):(20);
-}
-/*
-	cin.get();
-	cout<<int('A')-64<<endl;
-	satellite();
-	varun();
-	cout<<"\nUSACO\n";
-	main2();
 	return 0;
-	 */
+}
