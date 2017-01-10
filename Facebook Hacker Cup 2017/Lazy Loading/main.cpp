c0f708ae5e7c435f2dc9c22d948831245e33bdbf
Varun Sharma <contactvarun27@gmail.com>
2017-01-10 16:23:02 +0530

    Create main.cpp

---
+++ Facebook Hacker Cup 2017/Lazy Loading/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <deque>
+#include"LazyLoading.h"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+  lazy();
+	return 0;
+}
