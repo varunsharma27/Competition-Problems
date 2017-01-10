70d73b51dad44c0be36d26b9ee86b21c29e34dfe
Varun Sharma <contactvarun27@gmail.com>
2017-01-10 16:17:17 +0530

    Create main.cpp

---
+++ Facebook Hacker Cup 2017/Progress Pie/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <deque>
+#include"ProgressPie.h"
+
+
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    pie();
+    return 0;
+}
